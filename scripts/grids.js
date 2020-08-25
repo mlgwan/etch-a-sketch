5505d23b37d0504145400cf3dc8199dc8c9cbe41
Ciwan Clifford <theciwan@gmail.com>
2020-08-25 19:59:24 +0200

    Added a button to toggle color modes and added a color mode where the squares gradually become darker

--- scripts/grids.js
+++ scripts/grids.js
@@ -1,7 +1,11 @@
 const canvas = document.getElementById("canvas");
 const clearBtn = document.getElementById("clear-btn");
+const toggleColorBtn = document.getElementById("toggle-color-btn");
 let gridSize = 16;
 clearBtn.addEventListener("click", clearCanvas);
+toggleColorBtn.addEventListener("click", toggleColorMode);
+let colorMode = 0;
+const COLORMODE_AMOUNT = 2;
 
 function createCanvas() {
     for (let i = 0; i < gridSize; i++){
@@ -29,10 +33,14 @@ function createCanvas() {
 
 
 function markSquare(e){
-    let red= Math.ceil(Math.random()*255); 
-    let blue= Math.ceil(Math.random()*255); 
-    let green= Math.ceil(Math.random()*255); 
-    e.target.style.backgroundColor = `rgb(${red},${blue},${green})`;  
+    switch(colorMode){
+        case 0:
+            e.target.style.backgroundColor = randomColor();    
+            break;
+        case 1:
+            e.target.style.backgroundColor = blackerEveryHover(e.target.style.backgroundColor);
+        }
+      
 }
 
 function clearCanvas(){
@@ -46,6 +54,33 @@ function clearCanvas(){
         gridSize = parseInt(input);
         createCanvas();
     }
+}
+
+function toggleColorMode(){
+    colorMode++;
+    if (colorMode >= COLORMODE_AMOUNT){
+        colorMode = 0;
+    }
+}
+
+function randomColor(){
+    let red= Math.ceil(Math.random()*255); 
+    let blue= Math.ceil(Math.random()*255); 
+    let green= Math.ceil(Math.random()*255);
+    return `rgb(${red},${blue},${green})`;
+}
+
+function blackerEveryHover(color){
+    if (color == ""){
+        return "rgb(230,230,230)";
+    }
+
+    let temp = color.slice(4, color.length-1);
+    temp = temp.split(", ");
+    let blackerRed = parseInt(temp[0]) - 25;
+    let blackerBlue = parseInt(temp[1]) - 25;
+    let blackerGreen = parseInt(temp[2]) - 25;
+    return `rgb(${blackerRed},${blackerBlue},${blackerGreen})`;
 }
 
 createCanvas();
