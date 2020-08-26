65ad44999971532ddd57654bcd16c61c91e7e96a
Ciwan Clifford <theciwan@gmail.com>
2020-08-26 09:37:38 +0200

    Fixed the scaling issue, an input bug and added a regular color mode. Also put a lot of time into styling.

--- scripts/grids.js
+++ scripts/grids.js
@@ -2,52 +2,58 @@ const canvas = document.getElementById("canvas");
 const clearBtn = document.getElementById("clear-btn");
 const toggleColorBtn = document.getElementById("toggle-color-btn");
 let gridSize = 16;
+const CANVAS_SIZE = 500;
+let maxGridAmount;
 clearBtn.addEventListener("click", clearCanvas);
 toggleColorBtn.addEventListener("click", toggleColorMode);
 let colorMode = 0;
-const COLORMODE_AMOUNT = 2;
+const COLORMODE_AMOUNT = 3;
 
 function createCanvas() {
     for (let i = 0; i < gridSize; i++){
         const column = document.createElement("div");
         column.classList.add("column");
-        if (gridSize === 1){
-            column.classList.add("left-border");
-            column.classList.add("right-border");
-        }
-        else if (i === 0){
-            column.classList.add("left-border");
-        }
-        else if (i === gridSize-1){
-            column.classList.add("right-border");
-        }
         for (let j = 0; j < gridSize; j++){
             const square = document.createElement("div");
             square.classList.add("square");
+            let squareSize =  CANVAS_SIZE / (gridSize);
+            square.style.width = `${squareSize}px`;
+            square.style.height = `${squareSize}px`;
             square.addEventListener("mouseenter", markSquare);
             column.appendChild(square);
         }
         canvas.appendChild(column);
     }
+    maxGridAmount = CANVAS_SIZE / 10;
 }
 
 
 function markSquare(e){
     switch(colorMode){
         case 0:
+            e.target.style.backgroundColor = "black";
+            break; 
+        case 1:
             e.target.style.backgroundColor = randomColor();    
             break;
-        case 1:
-            e.target.style.backgroundColor = blackerEveryHover(e.target.style.backgroundColor);
+        case 2:
+           e.target.style.backgroundColor = blackerEveryHover(e.target.style.backgroundColor);
+            break;
         }
+
       
 }
 
 function clearCanvas(){
     canvas.innerHTML = "";
     let input = prompt("How big should your canvas be?");
-    if (typeof parseInt(input) != "number"){
+
+    if (isNaN(parseInt(input))){    
         alert("Please input a number");
+        clearCanvas();
+    }
+    else if (parseInt(input) > maxGridAmount){
+        alert(`Please pick a number between 1 and ${maxGridAmount}`);
         clearCanvas();
     }
     else{
